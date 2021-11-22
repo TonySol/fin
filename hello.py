f0338c2d0c83b2e4d0f2a5e2b31a8ea777f07505
TonySol <ortege@gmail.com>
2021-11-22 22:14:15 +0200

    added base app structure + flask-bootstrap module

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, dev-lunix World!</h1>"
+@app.route("/index")
+def index():
+    return render_template("index.html", title="Homepage", pagename="homepage", footer="link")
+
+if __name__ == "__main__":
+    app.run(debug=True)
