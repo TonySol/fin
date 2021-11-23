e99c5ea6dc998d8e6c6a2ca4e87298ec357eb8f4
TonySol <ortege@gmail.com>
2021-11-23 13:59:01 +0200

    two pages bo0tstrapp 5.1 added via base.html

--- hello.py
+++ hello.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-Bootstrap(app)
+
+
+
+menu = {'Home': '/index', "Brand": "/brands"}
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title="Homepage", pagename="homepage", footer="link")
+    return render_template("index.html", menu=menu, title="Homepage", pagename="homepage", footer="link")
+
+@app.route("/brands")
+def brands():
+    return render_template("brands.html", menu=menu, title="Brands", pagename="coffee brands", footer="link")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
