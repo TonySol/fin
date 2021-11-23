bc065f502a5e10746893dee6f82084413db79014
TonySol <ortege@gmail.com>
2021-11-23 23:14:11 +0200

    added post check for default page loading + flash message upon submit

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
 
-
+app.secret_key = "&F)J@NcCfUjXn2r5u8x/y?D(G-KaPdSb"
 menu = {'Home': 'index', "Brand": "brands", "Product": "product" }
 
 @app.route("/")
@@ -16,10 +16,13 @@ def brands():
 
 @app.route("/product", methods=["GET", "POST"])
 def product():
-    if request.form.getlist("rate") == ["1-star"]:
-        return render_template("index.html", menu=menu, title="Homepage", pagename="1 star product page", footer="link")
-    elif request.form.getlist("rate") == ["3-star"]:
-        return render_template("index.html", menu=menu, title="Homepage", pagename="3 star products page", footer="link")
+    if request.method == "POST":
+        if request.form.getlist("rate") == ["1-star"]:
+            flash("You poor thing, get your 1 star products.")
+            return render_template("product.html", menu=menu, title="Homepage", pagename="1 star product page", footer="link")
+        elif request.form.getlist("rate") == ["3-star"]:
+            flash("Here are your 3 star products, you little snob.")
+            return render_template("product.html", menu=menu, title="Homepage", pagename="3 star products page", footer="link")
     return render_template("product.html", menu=menu, title="Brands", pagename="prodcuts", footer="link")
 
 if __name__ == "__main__":
