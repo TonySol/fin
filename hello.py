0a15fa0d28558ec5a33367f97a1d4adef8f6241f
TonySol <ortege@gmail.com>
2021-11-23 22:16:16 +0200

    post for a search rating system + custom styles file

--- hello.py
+++ hello.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
-
-menu = {'Home': '/index', "Brand": "/brands"}
+menu = {'Home': 'index', "Brand": "brands"}
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", menu=menu, title="Homepage", pagename="homepage", footer="link")
 
-@app.route("/brands")
+@app.route("/brands", methods=["GET", "POST"])
 def brands():
+    if request.form.getlist("rate") == ["1-star"]:
+        return render_template("index.html", menu=menu, title="Homepage", pagename="homepage", footer="link")
+    elif request.form.getlist("rate") == ["3-star"]:
+        return render_template("index.html", menu=menu, title="Homepage", pagename="3 start page", footer="link")
     return render_template("brands.html", menu=menu, title="Brands", pagename="coffee brands", footer="link")
 
 
