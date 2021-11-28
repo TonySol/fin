980dd0eeae600e43d7b8a23b7041e620cd4b28a6
TonySol <ortege@gmail.com>
2021-11-28 21:36:18 +0200

    made a little factory for basic views instead of using blueprints

--- init.py
+++ init.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "dslkfneklf3pojfkojdenvfp03023rn"
 
-from views.routes import go
 
-app.register_blueprint(go)
+from views.routes import start_app
+start_app(app)
+
+
+from views.api_routes import api
+app.register_blueprint(api)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
