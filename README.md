d2e89aa5991874ee3a30d168fb32aa71f863926a
TonySol <ortege@gmail.com>
2021-12-05 16:26:54 +0200

    token added

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+[![Build Status](https://app.travis-ci.com/TonySol/fin.svg?branch=main)](https://app.travis-ci.com/TonySol/fin)
--- auto-merge.sh
+++ auto-merge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl --location --request PUT 'https://api.github.com/repos/'"$TRAVIS_REPO_SLUG"'/pulls/'"$TRAVIS_PULL_REQUEST"'/merge' \
---header 'Authorization: Bearer '"$GH_TOKEN"'' \
+--header 'Authorization: Bearer '"$GIT_TOKEN"'' \
