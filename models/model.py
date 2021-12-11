8b50fa285a289f9eb50247fc2566396d639534c0
TonySol <ortege@gmail.com>
2021-12-11 23:31:57 +0200

    models updated, migration made

--- models/model.py
+++ models/model.py
@@ -1,11 +1,24 @@
 import sys
+
 sys.path.append("..")
 from init import db
 
+
 class Department(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    descr = db.Column(db.String(50))
+    name = db.Column(db.String(20), primary_key=True, index=True, unique=True)
+    employees = db.relationship('Employee', backref='department', lazy=True)
 
     def __repr__(self):
-        return f"Name of dept. – {self.name}, with id – {self.id}."
+        return self.name
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    surname = db.Column(db.String(20))
+    date_of_bidth = db.Column(db.Date, index=True)
+    salary = db.Column(db.Integer, nullable=False)
+    dept_name = db.Column(db.String(20), db.ForeignKey('department.name'), nullable=False)
+
+    def __repr__(self):
+        return f"This is employee. – {self.name}, {self.surname} from dept – {self.dept_name}."
