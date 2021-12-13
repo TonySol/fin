d2b6cb0e4a8cc2d7b49c6794f57eaec4398653ab
TonySol <ortege@gmail.com>
2021-12-13 18:03:29 +0200

    added pagination to department detail page

--- views/routes.py
+++ views/routes.py
@@ -21,20 +21,18 @@ def routes(app, db, model):
                                pagename="Homepage",
                                footer=footer)
 
-
-
     @app.route("/departments", defaults={'page_num': 1})
     @app.route("/departments/page/<int:page_num>")
     def departments(page_num):
         Department = model.Department
         Employee = model.Employee
 
-        dept_salary = db.session.query(Department.name, func.avg(Employee.salary))\
-                            .select_from(Department).join(Employee)\
-                            .group_by(Department.name)\
-                            .paginate(per_page=2, page=page_num, error_out=True)\
-
-        # dept_salary = Department.query.\
+        dept_salary = db.session.query(Department.name, func.avg(Employee.salary)) \
+            .select_from(Department).join(Employee) \
+            .group_by(Department.name) \
+            .paginate(per_page=2, page=page_num, error_out=True) \
+ \
+                # dept_salary = Department.query.\
         #                     join(Employee, Department.name == Employee.dept_name)\
         #                     .add_columns(func.avg(Employee.salary))\
         #                     .group_by(Department.name)\
@@ -44,24 +42,23 @@ def routes(app, db, model):
                                menu=menu, title="Departments",
                                pagename="departments", footer="link")
 
-
-    @app.route("/department/<dept_name>")
-    def department(dept_name):
+    @app.route("/department/<dept_name>/", defaults={'page_num': 1})
+    @app.route("/department/<dept_name>/page/<int:page_num>")
+    def department(dept_name, page_num):
         Department = model.Department
         Employee = model.Employee
 
-
         dept_name_db = Department.query.filter_by(name=dept_name).first_or_404().name
-        dept_data = db.session.query\
-                        (Employee.name, Employee.surname, Employee.date_of_bidth, Employee.salary)\
-                        .select_from(Employee).filter_by(dept_name=dept_name)\
-                        .paginate(per_page=2, page=1, error_out=True)\
+        dept_data = db.session.query \
+                    (Employee.name, Employee.surname, Employee.date_of_bidth, Employee.salary) \
+                    .select_from(Employee).filter_by(dept_name=dept_name) \
+                    .paginate(per_page=2, page=page_num, error_out=True) \
 
-        return render_template("department.html", menu=menu, dept_data=dept_data,
+        return render_template("department.html", menu=menu, dept_name=dept_name,
+                               dept_data=dept_data,
                                body=dept_name_db.capitalize(),
                                title=dept_name_db.capitalize(),
                                footer="link")
-
 
     @app.route("/employees")
     def employees():
@@ -69,7 +66,8 @@ def routes(app, db, model):
         person = model.Employee.query.all()
         emp_list = [i for i in person]
 
-        return render_template("employees.html", column_names=column_names, emp_list=emp_list, menu=menu,
+        return render_template("employees.html", column_names=column_names, emp_list=emp_list,
+                               menu=menu,
                                title="List of all employees", pagename="employee list",
                                footer="link")
 
