9678dc9c7835be50a207fbfc07b6c5072928d254
TonySol <ortege@gmail.com>
2021-12-17 02:02:04 +0200

    added cascade on delete of parent

--- models/model.py
+++ models/model.py
@@ -7,7 +7,7 @@ from init import db
 class Department(db.Model):
     """Parent table for Employee"""
     name = db.Column(db.String(20), primary_key=True, index=True, unique=True)
-    employees = db.relationship('Employee', backref='department', lazy=True)
+    employees = db.relationship('Employee', backref='department', passive_deletes=True,  lazy=True)
 
     def __repr__(self):
         return self.name
@@ -19,11 +19,11 @@ class Employee(db.Model):
     :dept_name: is a foreign key for a Department table
     """
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    surname = db.Column(db.String(20))
+    name = db.Column(db.String(20), nullable=False)
+    surname = db.Column(db.String(20), nullable=False)
     date_of_bidth = db.Column(db.Date, index=True)
     salary = db.Column(db.Integer, nullable=False)
-    dept_name = db.Column(db.String(20), db.ForeignKey('department.name'), nullable=False)
+    dept_name = db.Column(db.String(20), db.ForeignKey('department.name', ondelete='CASCADE'))
 
     def __repr__(self):
         return f"This is employee. – {self.name}, {self.surname} from dept – {self.dept_name}."
