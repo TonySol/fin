1043304efc25ad5329edce0f60ef607d8817e843
TonySol <ortege@gmail.com>
2022-01-06 01:36:33 +0200

    added restful flask registration + logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@
 :env: choose production class or dev(uses Public HOST) class
 :routes: api-routes via blueprint, user-routes via method aka constructor
 """
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -30,8 +34,20 @@ def start_app(config_option):
     from app.views import web
     app.register_blueprint(web)
 
-    from app.views import api
-    app.register_blueprint(api, url_prefix="/api")
+    from app.rest import api_bp
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+
+    if config_option.__name__ == "Config":
+        file_handler = RotatingFileHandler('fin.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Fin_project started')
 
     return app
 
